049cd21934debe45a08b7fb696d97e79cd7533e9
Peter Galimutti <peter.prabhat@gmail.com>
2024-02-16 01:26:58 +0530

    V2 - ignore alphanumeric, convert case to lower case

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,4 +1,11 @@
-def is_palindrome(text):
-    return(print(text == text[::-1]))
+def is_palindrome(t):
+# - Iterates over each character in the input string 't'.
+# - Checks if the character is alphanumeric (a letter or a digit).
+# - Converts each character to lowercase.
+# - Joins the filtered characters into a single string, removing non-alphanumeric characters and ensuring uniformity in case.
+# - 'cleanse_t' contains the cleaned string without spaces, punctuation, and with all letters converted to lowercase.
 
-is_palindrome("radar")
+    cleanse_t = ''.join(text.lower() for text in t if text.isalnum())
+    return(print(cleanse_t == cleanse_t[::-1]))
+
+is_palindrome("A man, a plan, a canal, Panama")
